6e61c359150b5f7613825e4fcd27ead5ca3e249d
Touhid Zaman <touhidz1995@gmail.com>
2024-02-29 08:45:19 +0600

    Reduced duplicate codes and optimized

--- wp-content/themes/fictionaluniversity/functions.php
+++ wp-content/themes/fictionaluniversity/functions.php
@@ -1,6 +1,55 @@
 <?php
 
 
+// Reusable Page Banner
+function pageBanner($args = NULL)
+{
+    // PHP logic will live here
+    if (!isset($args['title'])) {
+        $args['title'] = get_the_title();
+    }
+
+    if (!isset($args['subtitle'])) {
+        $args['subtitle'] = get_field('page_banner_subtitle');
+    }
+
+    if (!isset($args['photo'])) {
+        if (get_field('page_banner_background_image')) {
+            $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+        } else {
+            $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+        }
+    }
+?>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(
+            <?php
+            // $pageBannerImage = get_field('page_banner_background_image');
+            // echo $pageBannerImage['sizes']['pageBanner'];
+            echo $args['photo'];
+            ?>)">
+        </div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title">
+                <?php
+                // the_title();
+                echo $args['title'];
+                ?>
+            </h1>
+            <div class="page-banner__intro">
+                <p>
+                    <?php
+                    // the_field('page_banner_subtitle');
+                    echo $args['subtitle'];
+                    ?>
+                </p>
+            </div>
+        </div>
+    </div>
+
+<?php
+}
+
 // Enqueueing Scripts
 function university_files()
 {
--- wp-content/themes/fictionaluniversity/page.php
+++ wp-content/themes/fictionaluniversity/page.php
@@ -3,16 +3,8 @@ get_header();
 
 while (have_posts()) {
     the_post();
+    pageBanner();
 ?>
-    <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"><?php the_title(); ?></h1>
-            <div class="page-banner__intro">
-                <p>DONT FORGET ME TO REPLACE ME LATER</p>
-            </div>
-        </div>
-    </div>
 
     <div class="container container--narrow page-section">
 
@@ -42,8 +34,6 @@ while (have_posts()) {
             <div class="page-links">
                 <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
                 <ul class="min-list">
-                    <!-- <li class="current_page_item"><a href="#">Our History</a></li>
-                <li><a href="#">Our Goals</a></li> -->
                     <?php
                     if ($theParent) {
                         $findChildrenOf = $theParent;
