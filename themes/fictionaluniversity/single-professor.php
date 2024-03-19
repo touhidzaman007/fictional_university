b057abb49316e19e394a2b82c6c39dde9a6ce2c1
Touhid Zaman <touhidz1995@gmail.com>
2024-03-20 00:07:50 +0600

    Started working on Like Btn

--- themes/fictionaluniversity/single-professor.php
+++ themes/fictionaluniversity/single-professor.php
@@ -15,6 +15,11 @@ while (have_posts()) {
                     ?>
                 </div>
                 <div class="two-thirds">
+                    <span class="like-box">
+                        <i class="fa fa-heart-o" aria-hidden="true"></i>
+                        <i class="fa fa-heart" aria-hidden="true"></i>
+                        <span class="like-count">3</span>
+                    </span>
                     <?php
                     the_content();
                     ?>
